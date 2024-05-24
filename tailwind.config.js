b7b461a6195497c466b4f4d51559be8764a9ae2e
HinYong1031 <hinyong16@gmail.com>
2024-05-24 19:22:07 +0800

    added dark mode switcher

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,13 @@ const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    flowbite.content()
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require("daisyui"),
-    require('flowbite/plugin')
-  ],
-}
-
+    content: ["./src/**/*.{js,jsx,ts,tsx}", flowbite.content()],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui"), require("flowbite/plugin")],
+    daisyui: {
+        themes: ["light", "dark", "dracula"],
+    },
+    darkMode: ["selector", "[data-theme='dracula']"],
+};
