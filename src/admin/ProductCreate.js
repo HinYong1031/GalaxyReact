85f0140771e9b27557142971659ca46f93ccfeb3
HinYong1031 <hinyong16@gmail.com>
2024-06-16 01:40:20 +0800

    complete copy sample create form for add product page

--- src/admin/ProductCreate.js
+++ src/admin/ProductCreate.js
@@ -1,10 +1,158 @@
+import { Button } from "flowbite-react";
+import { useState } from "react";
+
 function ProductCreate() {
+    const [formData, setFormData] = useState({
+        name: "",
+        brand: "",
+        price: "",
+        category: "",
+        itemWeight: "",
+        description: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission
+        console.log(formData);
+    };
     return (
         <section>
-            <div className="bg-white dark:bg-gray-800 pl-4">
-                <h1 className="mb-4 text-2xl font-bold text-gray-800 dark:text-white">
-                    Add a new product
+            <div className="bg-white dark:bg-gray-800 pt-2 pl-4">
+                <h1 className="text-2xl font-bold text-gray-800 dark:text-white">
+                    Add Product
                 </h1>
+                <div className="divider divider-primary"></div>
+                <form onSubmit={handleSubmit}>
+                    <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                        <div className="sm:col-span-2">
+                            <label
+                                htmlFor="name"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Product Name
+                            </label>
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                placeholder="Type product name"
+                                value={formData.name}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className="w-full">
+                            <label
+                                htmlFor="brand"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Brand
+                            </label>
+                            <input
+                                type="text"
+                                name="brand"
+                                id="brand"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                placeholder="Product brand"
+                                value={formData.brand}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className="w-full">
+                            <label
+                                htmlFor="price"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Price
+                            </label>
+                            <input
+                                type="number"
+                                name="price"
+                                id="price"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                placeholder="$2999"
+                                value={formData.price}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label
+                                htmlFor="category"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Category
+                            </label>
+                            <select
+                                id="category"
+                                name="category"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                value={formData.category}
+                                onChange={handleChange}
+                            >
+                                <option value="">Select category</option>
+                                <option value="TV">TV/Monitors</option>
+                                <option value="PC">PC</option>
+                                <option value="GA">Gaming/Console</option>
+                                <option value="PH">Phones</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label
+                                htmlFor="itemWeight"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Item Weight (kg)
+                            </label>
+                            <input
+                                type="number"
+                                name="itemWeight"
+                                id="itemWeight"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                placeholder="12"
+                                value={formData.itemWeight}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className="sm:col-span-2">
+                            <label
+                                htmlFor="description"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Description
+                            </label>
+                            <textarea
+                                id="description"
+                                name="description"
+                                rows="5"
+                                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                placeholder="Your description here"
+                                value={formData.description}
+                                onChange={handleChange}
+                            ></textarea>
+                        </div>
+                    </div>
+                    <div className="grid justify-items-end">
+                        <button
+                            type="submit"
+                            className="btn btn-primary px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+                        >
+                            Add product
+                        </button>
+                    </div>
+                </form>
             </div>
         </section>
     );
